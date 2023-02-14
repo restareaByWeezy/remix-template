d3c95d1a1774f0fe501bd0513f0a9c7f9f247fb1
restareaByWeezy <fjgkd7777@naver.com>
2023-02-14 23:44:12 +0900

    🔌 Config(lint): lint init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,33 @@
-/** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: ['react-app', 'react-app/jest', 'airbnb-typescript', 'prettier'],
+  plugins: ['json-format', 'simple-import-sort'],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  settings: {
+    'import/resolver': {
+      node: {},
+      typescript: {
+        directory: './src',
+      },
+    },
+    'import/parsers': { '@typescript-eslint/parser': ['.ts', '.tsx'] },
+  },
+  rules: {
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'import/extensions': ['off'],
+    '@typescript-eslint/no-unused-vars':
+      process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-console': 'error',
+    'react/jsx-key': 'error',
+  },
+  ignorePatterns: [
+    '**/__generated__/**/*',
+    '**/dist/**/*',
+    '.eslintrc.js',
+    'cz-config.js',
+  ],
 };
